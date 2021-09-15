1b8912a5fabd41e52b01ffb6071fadc2647bc167
codegitpro <michael.jin.dev@gmail.com>
2021-09-15 07:17:04 -0700

    update package scripts

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const main_data = require("./main/main-data")
+const docs_data = require("./docs")
 
 console.log("main_data", main_data)
+
+if (docs_data) {
+  console.log("docs_data", docs_data)
+} else {
+  console.log("no docs data available")
+}
